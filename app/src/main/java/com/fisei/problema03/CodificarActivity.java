ab947e36aea980dfca1570905227158afc3c115b
santisebas97 <nunezsantiago1e@gmail.com>
2021-12-03 23:12:53 -0500

    commit 24h00

---
+++ app/src/main/java/com/fisei/problema03/CodificarActivity.java
@@ -0,0 +1,14 @@
+package com.fisei.problema03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodificarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codificar2);
+    }
+}
